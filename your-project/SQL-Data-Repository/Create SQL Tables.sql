79fc13b5d7bb9402ab100a8f5e4eef3b7b0cbcf6
tristar82 <56124104+tristar82@users.noreply.github.com>
2019-11-14 15:21:40 +0100

    added tableau folder

--- your-project/SQL-Data-Repository/Create SQL Tables.sql
+++ your-project/SQL-Data-Repository/Create SQL Tables.sql
@@ -21,3 +21,18 @@ Channel VARCHAR(90) NOT NULL  ,
 last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 select * from content_creator;
+
+
+
+
+-- Newest Table
+drop table video_table;
+CREATE TABLE video_table (
+vid_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Channel VARCHAR(90) NOT NULL,
+Video_id VARCHAR(90) NOT NULL,
+Published VARCHAR(15), 
+Duration INT,
+last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+select * from video_table;
